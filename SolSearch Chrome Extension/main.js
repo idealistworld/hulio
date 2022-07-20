f22db97dbf1f8c440b13390ff2c1b25e4adecbb0
km199 <kai.mcgrath@me.com>
2022-07-20 12:45:33 -0700

    Reorganized the code into functions.

--- SolSearch Chrome Extension/main.js
+++ SolSearch Chrome Extension/main.js
@@ -20,6 +20,7 @@ function updateSettings() {
 }
 updateSettings();
 
+
 //Set the var url to current url
 const url = window.location.href;
 
@@ -29,42 +30,77 @@ const regex = /\/\/(www.)?solend.fi\/|\/\/(www.)?jup.ag\/|\/\/(www.)?orca.so\//g
 //Use the filter
 const found = url.match(regex);
 
+
+//Detection Algorithm
+//Detect if a website is Solana related
+//Only runs if url is not matched
+function detectSolana() {
+  //Scrape all the websites text and make it lowercase.
+  var content = document.body.textContent || document.body.innerText;
+  var contentLowerCase = content.toLowerCase()
+  //Terms
+  var hasSolana = contentLowerCase.indexOf("solana")!==-1;
+  var hasSol = contentLowerCase.indexOf(" sol ")!==-1;
+  var hasConnectWallet = contentLowerCase.indexOf("connect wallet")!==-1;
+  var hasLaunchApp = contentLowerCase.indexOf("launch app")!==-1;
+  //Apply Weights to Terms
+  //Needs to be calculated
+  var totalScore = hasSolana + hasSol + hasConnectWallet + hasLaunchApp
+  //Debuging
+  if (debugCBox) {
+    alert("Total score: " + totalScore + "\nSolana: " + hasSolana + "\nSol: " + hasSol + "\nConnect Wallet: " + hasConnectWallet + "\nLaunch App: " + hasLaunchApp)
+  }
+  //If score is above n, alert user that this is a potentially unsafe solana website. 
+  if (totalScore >= 2) {
+    warning();
+  } 
+}
+
+
+//Warning function, runs on websites that are solana related but unknown
+//Only runs if warningCBox is checked
+function warning() {
+  if (retypingCBox) {
+    //These are just placeholders for now
+    alert("Retyping placeholder")
+  }
+  else {
+    //Another placeholder
+    alert("Normal warning placeholder")
+  }
+}
+
+
+//SafePopup Function
+//Runs on all urls that are matched
+function safePopup () {
+  //Create our safe popup
+  var div = document.createElement("div");
+  //This HTML is inserted at the top of the body. 
+  div.innerHTML =
+    '<div class="popupSafe" id="popupSafe-1">\n' +
+      '<div class="overlay"></div>\n' +
+      '<div class="content">\n' +
+        '<p>This website is secure. Click this link to learn more.</p>\n' +
+      '</div>\n' + 
+    '</div>\n'
+  document.body.prepend(div);
+  //Fade out after a few seconds
+  //Needs to be coded, and css needs an overhaul
+}
+
+
+//Main logic of the program
 //Delay 2 seconds to ensure that the website is fully loaded before searching through it
 setTimeout(function(){
   //If the website is not matched by the regex filter
   if (found === null) {
-    var content = document.body.textContent || document.body.innerText;
-    var contentLowerCase = content.toLowerCase()
-    var hasSolana = contentLowerCase.indexOf("solana")!==-1;
-    var hasSol = contentLowerCase.indexOf(" sol ")!==-1;
-    var hasConnectWallet = contentLowerCase.indexOf("connect wallet")!==-1;
-    var hasLaunchApp = contentLowerCase.indexOf("launch app")!==-1;
-    //Debuging
-    if (debugCBox) {
-      alert("Total score: " + (hasSolana + hasSol + hasConnectWallet + hasLaunchApp) + "\nSolana: " + hasSolana + "\nSol: " + hasSol + "\nConnect Wallet: " + hasConnectWallet + "\nLaunch App: " + hasLaunchApp)
-    }
-    //If score is above n, alert user that this is a potentially unsafe solana website. 
-    if (hasSolana + hasSol + hasConnectWallet + hasLaunchApp >= 2) {
-      //Temporary alert. This will be replaced by an popup thats on the webpage. 
-      alert("DANGER DANGER");
-    } 
-    else {
+    if (warningCBox) {
+      detectSolana();  
     }
   } 
   //If the website is matched by the regex filter. 
   else {
-    //Create our safe popup
-    var div = document.createElement("div");
-    //This HTML is inserted at the top of the body. 
-    div.innerHTML =
-      '<div class="popupSafe" id="popupSafe-1">\n' +
-        '<div class="overlay"></div>\n' +
-        '<div class="content">\n' +
-          '<p>This website is secure. Click this link to learn more.</p>\n' +
-        '</div>\n' + 
-      '</div>\n'
-    document.body.prepend(div);
-    //Fade out after a few seconds
-    //Needs to be coded
+    safePopup();
   }
 }, 2000);
