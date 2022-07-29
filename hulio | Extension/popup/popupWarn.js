2cec4345c691f81ff23f014903b525bafcbe6754
cnakayama24 <cnakayama567@gmail.com>
2022-07-28 20:34:05 -0700

    Site and Button Updates !

--- hulio | Extension/popup/popupWarn.js
+++ hulio | Extension/popup/popupWarn.js
@@ -5,7 +5,7 @@ function warnPopup () {
     var div = document.createElement("div");
     //This HTML is inserted at the top of the body. 
     div.innerHTML =
-      '<div id="popup-safe">\n' +
+      '<div id="popup-warn">\n' +
         '<div id="popup-safe-content">\n' +
         '<h1 id = "title123456">This site is not secure.</h1>\n' +
           '<p id = "paragraph123456">This website is secure. Click this link to learn more.</p>\n' +
