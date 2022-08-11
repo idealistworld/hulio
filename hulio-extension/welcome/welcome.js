e792eef7cda1be4e0be5cbde487a10693e3cc619
km199 <kai.mcgrath@me.com>
2022-08-10 19:49:15 -0700

    Welcome is working, remaking home

--- hulio-extension/welcome/welcome.js
+++ hulio-extension/welcome/welcome.js
@@ -3,7 +3,8 @@ function tutorial () {
 }
 
 function skip () {
-    alert("Skip");
+    var internalSettingsUrl = chrome.runtime.getURL("settings/settings.html");
+    window.open(internalSettingsUrl, "_self");
 }
 
 window.onload = function () {
@@ -17,3 +18,56 @@ window.onload = function () {
         el2.addEventListener('click', skip)
     }
 }
+
+//Load safe/ignore/unsafe lists
+//Update the list of safe sites
+function updateSafeSitesList (url) {
+    var safeSitesList;
+    var safeSitesListStr;
+    $.get(url, function( data ) {
+        safeSitesList = data.split("\n");
+        safeSitesListStr = data.toLowerCase();
+        chrome.storage.sync.set({
+          safeSitesList: safeSitesList,
+          safeSitesListStr: safeSitesListStr,
+        }, function() {});
+    });
+}
+
+function updateIgnoreSitesList (url) {
+    var ignoreSitesList;
+    var ignoreSitesListStr;
+    $.get(url, function (data) {
+        ignoreSitesList = data.split("\n");
+        ignoreSitesListStr = data.toLowerCase();
+        chrome.storage.sync.set({
+            ignoreSitesList: ignoreSitesList,
+            ignoreSitesListStr: ignoreSitesListStr,
+        }, function () {
+            checkLists()
+        });
+    });
+}
+
+//Check to ensure that the lists are not empty
+//Return true or false
+function checkLists () {
+    chrome.storage.sync.get({
+        safeSitesListStr: "",
+        ignoreSitesListStr: "",
+    }, function (items) {
+        if (!(items.safeSitesListStr.length && items.ignoreSitesListStr.length)) {
+            alert("Unable to acess the database, extension functionality may be impared. Reinstall or manually pull from the database in settings.");
+        }
+    });
+}
+
+function initLists () {
+    updateSafeSitesList("https://idealistworld.github.io/hulio/safeSitesList.txt");
+    updateIgnoreSitesList("https://idealistworld.github.io/hulio/ignoreSitesList.txt");
+}
+
+setTimeout(function () {
+    initLists();
+  }, 100);
+
