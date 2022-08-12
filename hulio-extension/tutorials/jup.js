3fad5b8c0b69f9401e103ba0c0f5fd4d7b356b73
km199 <kai.mcgrath@me.com>
2022-08-11 23:18:44 -0700

    Working on jup tutorial

--- hulio-extension/tutorials/jup.js
+++ hulio-extension/tutorials/jup.js
@@ -15,8 +15,10 @@ function incrementOnClick () {
 function checkCrypto (elem, crypto) {
     el = document.querySelector(elem)
     if (el.innerHTML === crypto) {
+        alert('Solana detected');
         increment();
     } else {
+        step = step -1
         checkStep();
     }
 }
@@ -90,18 +92,50 @@ const checkStep = () => {
 
     else if (step === 6) {
         popup.innerHTML =
-            '<div class="popup-tutorial-template popup-1">\n' +
+            '<div class="popup-tutorial-template popup-2">\n' +
             '<h1 id = "site-title">Sell Solana</h1>\n' +
-            "<p id = 'paragraph123456'>You recieved some Solana earlier. Now sell it. Choose it from the drop down menu.</p>\n" +
+            "<p id = 'paragraph123456'>You recieved some Solana earlier. Now let's say you think Solana is going to crash. Let's swap it for USDT (whats that) and then buy it back later. </p>\n" +
             "<button id='button-1'>I did</button>\n" +
             '</div>'
         document.getElementById("button-1").onclick = increment
+    }
+
+    else if (step === 7) {
+        checkCrypto ("#__next > div.flex.flex-col.min-h-screen.justify-between > div > div.flex.flex-col.justify-between > div > div.w-full.max-w-md > div.w-full.max-w-md.pb-6.md\\:pb-12.lg\\:pb-24 > form > div.w-full.rounded-xl.bg-white-75.dark\\:bg-white.dark\\:bg-opacity-5.shadow-lg.flex.flex-col.p-4.lg\\:px-6.lg\\:py-8 > div.border-b.border-transparent > div > div > div > div > button > div.ml-4.mr-2.font-semibold", 'SOL');
+    }
+
+    else if (step === 8) {
+        popup.innerHTML =
+            '<div class="popup-tutorial-template popup-2">\n' +
+            '<h1 id = "site-title">Swap to USDT</h1>\n' +
+            "<p id = 'paragraph123456'>Select USDT as the currency to recieve.</p>\n" +
+            "<button id='button-1'>I did</button>\n" +
+            '</div>'
+        document.getElementById("button-1").onclick = increment
+    }
+
+    else if (step === 9) {
+        checkCrypto ("#__next > div.flex.flex-col.min-h-screen.justify-between > div > div.flex.flex-col.justify-between > div > div.w-full.max-w-md > div.w-full.max-w-md.pb-6.md\\:pb-12.lg\\:pb-24 > form > div.w-full.rounded-xl.bg-white-75.dark\\:bg-white.dark\\:bg-opacity-5.shadow-lg.flex.flex-col.p-4.lg\\:px-6.lg\\:py-8 > div.px-3 > div > div > div > button > div.ml-4.mr-2.font-semibold", "USDT");
+    }
+
+    else if (step === 10) {
+        popup.innerHTML =
+            '<div class="popup-tutorial-template popup-2">\n' +
+            '<h1 id = "site-title">Click Swap</h1>\n' +
+            "<p id = 'paragraph123456'>Select USDT as the currency to recieve.</p>\n" +
+            "<button id='button-1'>I did</button>\n" +
+            '</div>'
+        document.getElementById("button-1").onclick = increment
+    }
+
+    else if (step === 11) {
+        
     }
 }
 
 
 
+setTimeout(function () {
+    checkStep();
+  }, 500);
 
-checkStep();
-
-//checkCrypto ('//*[@id="__next"]/div[1]/div/div[3]/div/div[3]/div[2]/form/div[1]/div[2]/div/div/div/div/button/div[2]', 'SOL');
