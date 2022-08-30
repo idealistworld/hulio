1f057a8fbb432d046c97370f407dfc62bbd50442
km199 <kai.mcgrath@me.com>
2022-08-29 17:00:21 -0700

    Cleaning up

--- hulio-extension/popup/popupWarnRetype.js
+++ hulio-extension/popup/popupWarnRetype.js
@@ -1,20 +1,16 @@
 //Chris's function
 //Retyping popup. 
 function retyping () {
-    var verified = false;
-    var risky = true;
-    
+    var verified = false;    
     var count = 0;
     
-    const close = () =>
-    {
-      if (verified)
-      {
+    const close = () => {
+      if (verified) {
         document.getElementById("popup12345").remove();
       }
     }
+
     function updateIgnoreWarnRetypeList() {
-      
       chrome.storage.local.get({
         ignoreWarnRetypeList: [],
       }, function(items) {
@@ -31,8 +27,7 @@ function retyping () {
       });
     }
 
-    const verify = () =>
-    {
+    const verify = () => {
       var input = document.getElementById("userInput12345").value;
       if (input === url)
       {
@@ -55,29 +50,15 @@ function retyping () {
       }
     }
     
-    const html =  '<div id="popup12345">\n' +
-    '<div id = "overlay12345"></div>\n' +
-    '<div id = "content12345">\n' +
-      '<h1 id = "title12345">This is a potentially risky site.</h1>\n' +
-      '<p id = "paragraph12345">This page is not in our database. Verify the URL before accessing it.</p>\n' +
-      `<input  autocomplete="off" id = "userInput12345" placeholder="example.com" type ="text"></input>\n` +
-    '</div>\n' +
-    '</div>\n';
-    
-    if(risky)
-    {
-      var poppingContent = document.createElement("div");
-      poppingContent.innerHTML = '<div id="popup12345">\n' +
+    var poppingContent = document.createElement("div");
+    poppingContent.innerHTML = '<div id="popup12345">\n' +
       '<div id = "overlay12345"></div>\n' +
       '<div id = "content12345">\n' +
-        '<h1 id = "title12345">This is a risky site.</h1>\n' +
+        '<h1 id = "title12345">This is a potentially risky site.</h1>\n' +
         '<p id = "paragraph12345">This page is not in our database. Verify the URL before accessing it.</p>\n' +
         `<input  autocomplete="off" id = "userInput12345" placeholder="example.com" type ="text"></input>\n` +
       '</div>\n' +
       '</div>\n'
-      document.body.prepend(poppingContent); 
-      document.getElementById("userInput12345").oninput = verify;
-    }
-
-
+    document.body.prepend(poppingContent); 
+    document.getElementById("userInput12345").oninput = verify;
 }
